eb1933826dd4b6c61b4adc58b68acdbaf85021f1
Sadgruru <73537027+sadhguru-slayer@users.noreply.github.com>
2025-12-29 23:23:31 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -32,14 +32,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-                    <Script id="gtm-script" strategy="beforeInteractive">
+                          <head>
+           <script>
           {`
           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-K294VQLX');
-`}</Script>
+`}</script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50 text-gray-900`}
