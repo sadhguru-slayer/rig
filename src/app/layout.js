159328c5ca6c2c553f0bc609b4692ddd245b1b99
Sadgruru <sadgurustreak@gmail.com>
2025-12-29 22:27:27 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,7 @@ import RoleBasedNavbar from "@/components/RoleBasedNavbar";
 import RoleBasedFooter from "@/components/RoleBasedFooter";
 import AppLayoutClient from "@/components/AppLayoutClient";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,11 +32,32 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"></script>
+        <Script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"></Script>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+        >{`
+          (function(w,d,s,l,i){w[l]=w[l]||[];
+          w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+          var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+          j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+          f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-K294VQLX');
+        `}</Script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50 text-gray-900`}
       >
+      <noscript>
+  <iframe
+    src="https://www.googletagmanager.com/ns.html?id=GTM-K294VQLX"
+    height="0"
+    width="0"
+    style={{ display: "none", visibility: "hidden" }}
+  />
+</noscript>
+
         <Toaster />
         {/* Client-side layout logic */}
         <AppLayoutClient>{children}</AppLayoutClient>
