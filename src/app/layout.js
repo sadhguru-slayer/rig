ec0c88ad2751b2772bf3b04783bf30a46bea59df
Sadgruru <73537027+sadhguru-slayer@users.noreply.github.com>
2025-12-29 23:15:58 +0530

    Change GTM script loading strategy to beforeInteractive

--- src/app/layout.js
+++ src/app/layout.js
@@ -32,7 +32,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-                    <Script id="gtm-script" strategy="afterInteractive">
+                    <Script id="gtm-script" strategy="beforeInteractive">
           {`
           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
