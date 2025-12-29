d5507d47a3aeb87de8ef125d1fee42f2162925bb
Sadgruru <73537027+sadhguru-slayer@users.noreply.github.com>
2025-12-29 23:11:57 +0530

    Refactor Google Tag Manager script to use Script component

--- src/app/layout.js
+++ src/app/layout.js
@@ -32,18 +32,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-          <script
-          id="gtm-script"
-          strategy="afterInteractive"
-        >{`
-          (function(w,d,s,l,i){w[l]=w[l]||[];
-          w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-          var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-          j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-          f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-K294VQLX');
-        `}</script>
+                    <Script id="gtm-script" strategy="afterInteractive">
+          {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K294VQLX');
+`}</Script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50 text-gray-900`}
